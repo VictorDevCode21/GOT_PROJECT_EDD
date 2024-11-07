df3b611e986f2d20408e1b494c1dd61a8f69b57e
Victor Rivas <victorenrique2002@gmail.com>
2024-11-06 23:00:36 -0400

    Repository and project created

---
+++ src/main/java/com/mycompany/game_of_thrones/GAME_OF_THRONES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.game_of_thrones;
+
+/**
+ *
+ * @author PC
+ */
+public class GAME_OF_THRONES {
+
+    public static void main(String[] args) {
+        System.out.println("Lineage of GAME OF THRONES!");
+    }
+}
