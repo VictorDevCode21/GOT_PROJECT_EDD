115ee7ff30a130c5a24d1950df0eecf90893c061
CarlosL010 <go.carlos@correo.unimet.edu.ve>
2024-11-23 23:59:40 -0400

    javadoc added to generic queue

--- src/main/java/edu/unimet/edd/tree/GenericQueue.java
+++ src/main/java/edu/unimet/edd/tree/GenericQueue.java
@@ -1,18 +1,41 @@
 package edu.unimet.edd.tree;
 
 
+/**
+ * A generic queue implemented with a linked list.
+ * 
+ *
+ * @param <T> The type of elements in the queue.
+ */
 public class GenericQueue<T> {
 
     private GenericLinkedList<T> list = new GenericLinkedList<>();
 
+    
+    /**
+     * Adds an item to the end of the queue.
+     * 
+     * @param item The item to add to the queue.
+     */
     public void enqueue(T item) {
         list.add(item);
     }
 
+    /**
+     * Removes and returns the item at the front of the queue.
+     * 
+     * @return The item at the front of the queue.
+     * 
+     */
     public T dequeue() {
         return list.removeFirst(); // Suponiendo que tienes un método para eliminar el primer elemento
     }
 
+    /**
+     * Checks if the queue is empty.
+     * 
+     * @return true if the queue is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return list.getSize() == 0;
     }
